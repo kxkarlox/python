fce6d24ec3fe7b609053bf877ebbc96798f3be29
kx.karlox <kx.karlox@gmail.com>
2022-06-16 19:17:35 -0400

    functions and module

--- functions.py
+++ functions.py
@@ -1,6 +1,2 @@
 def square(x):
-    return x*x
-
-
-for i in range(10):
-    print(f"the square of {i} is {square(i)}")
+    return x * x
--- squares.py
+++ squares.py
@@ -1,2 +1,4 @@
-def square1(x):
-    return x*x
+from functions import square
+
+for i in range(10):
+    print(f"the square of {i} is {square(i)}")
